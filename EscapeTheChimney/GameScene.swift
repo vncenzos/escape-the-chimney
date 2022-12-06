2cd6af214005f6aa6bed16e2e7ec1384be2005e2
vncenzos <vincenzo.dambrosio1999@gmail.com>
2022-12-06 19:49:26 +0100

    Sprites enabled, max speed regulator added
    The game is starting to work

--- EscapeTheChimney/GameScene.swift
+++ EscapeTheChimney/GameScene.swift
@@ -15,24 +15,25 @@ enum TouchState {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    var character = SKSpriteNode(imageNamed: "SantaPH")
     
+    var character = SKSpriteNode(imageNamed: "santa")
     var movespeed: Int = 5
-    
     var touchLocation: TouchState = .None
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
         //SantaCharacter
-        character.setScale(2)
+        character.setScale(1)
+        character.position = CGPoint(x:0, y:0)
         character.physicsBody = SKPhysicsBody(rectangleOf: character.size)
         character.physicsBody?.isDynamic = true
         character.physicsBody?.allowsRotation = false
-        self.addChild(character)
         character.physicsBody?.affectedByGravity = true
+        character.physicsBody?.friction = 1
+        character.physicsBody?.mass = 0.1
+        addChild(character)
     }
-    
     
     func touchDown(atPoint pos : CGPoint) {
     }
@@ -72,8 +73,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
     }
     
-    
     override func update(_ currentTime: TimeInterval) {
+        
+        // print(character.physicsBody?.velocity) // USE FOR DEBUG
+        // Max speed regulator
+        if ((character.physicsBody?.velocity.dx)! > 400.00) {
+            character.physicsBody?.velocity = CGVectorMake(400, 0);
+         }
+        
+        if ((character.physicsBody?.velocity.dx)! < -400.00) {
+            character.physicsBody?.velocity = CGVectorMake(-400, 0);
+         }
+        
         // Called before each frame is rendered
         switch (touchLocation) {
         case .Left:
@@ -83,5 +94,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         case .None:
             break
         }
+        
     }
+    
 }
+
+
