c31f300859cf495b9981378c11f1fc2e7458b98d
vncenzos <vincenzo.dambrosio1999@gmail.com>
2022-12-09 21:04:45 +0100

    IL GIOCO FUNZIONA PER ORA.
    WOW

--- EscapeTheChimney/GameScene.swift
+++ EscapeTheChimney/GameScene.swift
@@ -32,15 +32,17 @@ struct PhysicsCategory {
 }
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
-    
     //Dichiarazione telecamera
     let cam = SKCameraNode()
     
     //Dichiarare sprite qui
     
+    var platformGroup = [SKNode]()
     var platformNames = ["platform1", "platform2", "platform3", "platform4"]
     var santa = SKSpriteNode(imageNamed: "santa")
     
+    //Distribuzione randomica asse x
+    var randomPos = GKRandomDistribution(lowestValue: -320, highestValue: 320)
     
     //Dichiarare variabili gameplay qui
     var movespeed: Int = 5
@@ -49,12 +51,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     //Chiamata alla creazione della GameScene
     override func didMove(to view: SKView) {
-    
+        
         camera = cam //Assegnazione telecamera
         physicsWorld.contactDelegate = self  //Gestore delle collisioni
         
         //Creazione di Nabbo Batale
         makeSanta()
+        //Creazione di piattaforme iniziali
+        makePlatform()
+        makePlatform()
+        makePlatform()
     }
     //Chiamata quando tocchi lo schermo
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -89,10 +95,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //Chiamata prima di ogni frame renderizzato
     override func update(_ currentTime: TimeInterval) {
         //Aggiornamento telecamera
-        cam.setScale(CGFloat(1))
-        cam.position.y = santa.position.y + 500
+        cam.setScale(CGFloat(0.95))
+        cam.position.y = santa.position.y + 550
         cam.position.x = 0
         
+        //Condizione per far passare babbo attraverso le piattaforme da sotto ma non da sopra
+        if((santa.physicsBody?.velocity.dy)!>100){
+            santa.physicsBody?.collisionBitMask = 0
+        }
+        else
+        {
+            santa.physicsBody?.collisionBitMask = PhysicsCategory.Santa
+        }
+        
+        //Switch per direzione asse X
         // print(character.physicsBody?.velocity) // Usare per misure velocità personaggio
         switch (touchLocation) {
         case .Left:
@@ -103,6 +119,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             break
         }
         
+        //Switch per direzione asse Y (salto)
         switch (jumpState) {
         case .Jump:
             santa.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 100))
@@ -114,38 +131,37 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             break
         }
     }
-    
+    //Funzione morte di babbo
     func santaDeath() {
         santa.removeFromParent()
     }
     
     //Funzione attiva su ogni contatto tra oggetti
     func didBegin(_ contact: SKPhysicsContact) {
-            let collision = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask //Prende contatto di oggetto A e B
-                if collision == PhysicsCategory.Santa | PhysicsCategory.Cookie {
-                  //  cookieGet()
-                }
+        let collision = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask //Prende contatto di oggetto A e B
+        if collision == PhysicsCategory.Santa | PhysicsCategory.Cookie {
+            //  cookieGet()
+        }
         //Contatto tra babbo e fuoco = Morte
         if collision == PhysicsCategory.Santa | PhysicsCategory.Fire {
             print("Contact between santa and fire")
-                    santaDeath()
-                }
+            santaDeath()
+        }
         //Contatto tra babbo e piattaforma = Salto
         if collision == PhysicsCategory.Santa | PhysicsCategory.Spring {
             print("Contact between santa and spring")
             jumpState = .Jump
-                }
-            }
+        }
+    }
     //Regolatore velocità
     override func didSimulatePhysics() {
         let yVelocity : CGFloat? = santa.physicsBody?.velocity.dy
         if ((santa.physicsBody?.velocity.dx)! > 500.00) {
             santa.physicsBody?.velocity = CGVectorMake(500, yVelocity!);
          }
-        
         if ((santa.physicsBody?.velocity.dx)! < -500.00) {
             santa.physicsBody?.velocity = CGVectorMake(-500, yVelocity!);
-         }
+        }
     }
     
     //ENTITY LIST
@@ -153,7 +169,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     //Propietà di Babbo Natale
     func makeSanta(){
         
-        santa.position = CGPoint(x:0, y:-400)
+        santa.position = CGPoint(x:0, y:-600)
         santa.physicsBody = SKPhysicsBody(rectangleOf: santa.size)
         santa.physicsBody?.isDynamic = true
         santa.physicsBody?.allowsRotation = false
@@ -162,34 +178,48 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         santa.physicsBody?.restitution = 0
         santa.physicsBody?.mass = 0.1
         santa.physicsBody?.categoryBitMask = PhysicsCategory.Santa
+        santa.zPosition = 100
         addChild(santa)
         
     }
     
     //Proprietà delle piattaforme
-    func makePlatform(){
-        
+    func makePlatform() -> (){
         let platform = SKSpriteNode(imageNamed: platformNames.randomElement()!)
         platform.physicsBody = SKPhysicsBody(rectangleOf: platform.size)
         platform.physicsBody?.isDynamic = false
         platform.physicsBody?.allowsRotation = false
         platform.physicsBody?.affectedByGravity = false
         platform.physicsBody?.categoryBitMask = PhysicsCategory.Wall
-        platform.position = CGPoint(x: 0, y: santa.position.y+200)
+        platform.physicsBody?.collisionBitMask = 0
+        var randomX = randomPos.nextInt()
+        if platformGroup.isEmpty{
+            platform.position = CGPoint(x: randomX , y: Int(santa.position.y+250))
+        }
+        else
+        {
+            platform.position = CGPoint(x: randomX , y: Int(platformGroup.last!.position.y+250))
+        }
         print("Platform generated at \(platform.position)") //Test per posizione di generazione piattaforme
         addChild(platform)
         makeSpring(position: platform.position)
+        platformGroup.append(platform)
+        print(platformGroup.last)
     }
     
     func makeSpring(position: CGPoint) -> SKNode{
+        
         let spring = SKSpriteNode(imageNamed: "SpringPH")
+        spring.physicsBody = SKPhysicsBody(rectangleOf: spring.size)
         spring.position = CGPoint(x: position.x, y: position.y+20)
         spring.physicsBody?.isDynamic = false
         spring.physicsBody?.allowsRotation = false
         spring.physicsBody?.affectedByGravity = false
         spring.physicsBody?.categoryBitMask = PhysicsCategory.Spring
+        spring.physicsBody?.contactTestBitMask = PhysicsCategory.Spring
         print("Spring generated at \(spring.position)") //Test per posizione di generazione molle
         addChild(spring)
         return spring
+        
     }
 }
